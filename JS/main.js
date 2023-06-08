e813f205ffe115e48c3a0e28aa06f33b8b942754
Muzakialfarisi <muzakialfarisi3@gmail.com>
2023-06-08 07:23:56 +0700

    Update fitu fitur terbaru

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,47 @@
+// Toggle class active search form
+
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button-nav").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
 // Toggle Sidebar
 
 const navbarNav = document.querySelector(".navbar-nav");
 
 // Ketika Hamburger di klik muncul Side bar
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Toggle Shopping Cart
+
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
 };
 
 // Ketika klik diluar hamburger maka sidebarnya hilang
 const hamburger = document.querySelector("#hamburger-menu");
+const sbox = document.querySelector("#search-button-nav");
+const scart = document.querySelector("#shopping-cart");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sbox.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  if (!scart.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
   }
 });
